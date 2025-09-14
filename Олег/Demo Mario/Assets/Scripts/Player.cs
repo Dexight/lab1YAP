2faa9e6186deb9e6d483ad4a05ba9598d7966a2a
Dexight <tupikin@sfedu.ru>
2025-09-14 15:44:45 +0300

    init 1 course

---
+++ Олег/Demo Mario/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class Player : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
