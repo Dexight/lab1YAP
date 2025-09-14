39a608b66f7b061d52691c9e5488aa6ee5187324
Dexight <tupikin@sfedu.ru>
2025-09-14 20:59:51 +0300

    uncomplete 7 task

--- Олег/Demo Mario/Assets/Scripts/Enemy.cs
+++ Олег/Demo Mario/Assets/Scripts/Enemy.cs
@@ -7,11 +7,53 @@ using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    private Rigidbody2D rigidbody2d;
+    private Vector2 direction = Vector2.left;
+
+    private void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
     private void Start()
     {
+        rigidbody2d.velocity = direction * speed;
     }
 
     private void Update()
     {
+    }
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            bool dead = collision.contacts.All(c => c.point.y > transform.position.y);
+            if (dead)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+        else ChangeDirection(!collision.contacts.All(c => c.point.y < transform.position.y));
+    }
+
+    private void ChangeDirection(bool makeChange)
+    {
+        if (makeChange)
+        {
+            direction = -direction;
+            Vector2 velocity = rigidbody2d.velocity;
+            velocity.x = direction.x * speed;
+            rigidbody2d.velocity = velocity;
+        }
     }
 }
